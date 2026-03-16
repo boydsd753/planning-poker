306f6429c44a89b8bfaba59ec61d6edb5c7642a6
boydsd753 <boydsd753@gmail.com>
2026-03-16 14:41:38 -0400

    security

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -7,7 +7,7 @@ module.exports = function securityHeaders(req, res, next) {
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
   res.setHeader('Content-Security-Policy', [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline'",
+    "script-src 'self'",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data:",
